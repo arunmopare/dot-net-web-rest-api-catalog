f30f703dbc3ca6477dbed77ee36a8794c305080a
Arun Mopare <arunmopare546@gmail.com>
2021-06-29 22:41:56 +0530

    resolved bug added not found exeception

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Catalog.Entities;
@@ -22,6 +23,19 @@ namespace Catalog.Controller
         {
             var items = repository.GetItems();
             return items;
+        }
+
+        //GET /items/id
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+
+        {
+            var item = repository.GetItem(id);
+            if (item is null)
+            {
+                return NotFound();
+            }
+            return item;
         }
 
     }
